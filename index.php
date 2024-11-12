fe16e43f46718f74d975eea00b35a3046a4d0266
aida-home <aidasekarningrum23@mhs.politala.ac.id>
2024-11-12 15:19:38 +0800

    masukkan file ke repository

---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan</title>
+</head>
+<body>
+    <h1> INDONESIA RAYA </h1>
+    <?php
+    echo "Aida Sekar Ningrum";
+?>
+</body>
+</html>
